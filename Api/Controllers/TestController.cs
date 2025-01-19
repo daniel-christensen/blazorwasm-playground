d300bc42c8067d678590b69ec2680167c6b2ca37
Daniel Christensen <dchri18@gmail.com>
2025-01-19 12:59:49 +0100

    initial project commit Was cleaning up my project folder on my pc and found this project, uncommitted, but linked to a git repo. I am pushing this, but cannot verify wtf I was doing.

---
+++ Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Controller]
+    [Route("api/v1/[controller]/")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetMessage()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,20 @@
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            app.MapControllers();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.Run();
+        }
+    }
+}
